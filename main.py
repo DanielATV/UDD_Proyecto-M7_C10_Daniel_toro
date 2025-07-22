0ca5f708f678b0cc76d304c07864000355a771c0
DanielATV <daniel.torov@sansano.usm.cl>
2025-07-21 23:00:12 -0400

    modelo + residuos

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ from scipy.stats import norm
 import joblib
 
 
+
 app = Flask(__name__)
 
 print("🔄 Cargando modelo y residuos...")
-model = joblib.load('modelo.pkl')
-residuos = joblib.load('residual_std.pkl')
-residual_std = residuos['residual_std']
+model = joblib.load('best_model.pkl')
+residual_std = joblib.load('residual_std.pkl')
 print(f"✅ Todo cargado! Residual std: {residual_std:.4f}")
 
 @app.route('/predice', methods=['POST'])
 def predict():
-        
-         # Obtener datos
+
+        # Obtener datos
         data = request.json
         features = data['features']
         confidence = data.get('confidence', 0.95)
         # Predicción
         pred = model.predict([features])[0]
-        
+
         # Intervalo
         z_score = norm.ppf(1 - (1-confidence)/2)
         margin = z_score * residual_std
-        
+
         return {
         'prediction': float(pred),
         'lower': float(pred - margin),
