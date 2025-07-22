f81e4261de3a0caa991f30b590730afe2d5407e6
DanielATV <daniel.torov@sansano.usm.cl>
2025-07-21 23:10:22 -0400

    cambio para pythoneverywhere

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ def predict():
         }
 
 
-if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 4444)))
